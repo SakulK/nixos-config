d14ddf08a59187467a6a7273df1d2101e33d762c
sakulk <sakulk@gmail.com>
2021-05-18 07:55:59 +0200

    gnome 40

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -3,13 +3,13 @@
 {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   services.xserver.layout = "pl";
 
   environment.systemPackages = with pkgs; [
     dracula-theme
-    gnome3.gnome-tweaks
-    gnome3.gnome-shell-extensions
+    gnome.gnome-tweak-tool
+    gnome.gnome-shell-extensions
     gnomeExtensions.sound-output-device-chooser
     gnomeExtensions.appindicator
     gnomeExtensions.system-monitor
