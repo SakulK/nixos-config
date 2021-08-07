07a8a2e0e8991f1586b69534f2b2a5218997e9c8
sakulk <sakulk@gmail.com>
2021-08-07 22:49:06 +0200

    tweak bar

--- modules/i3.nix
+++ modules/i3.nix
@@ -128,29 +128,21 @@ in {
           theme = "gruvbox-dark";
           blocks = [
             {
-              block = "disk_space";
-              path = "/";
-              alias = "/";
-              info_type = "available";
-              unit = "GB";
-              interval = 60;
-              warning = 20.0;
-              alert = 10.0;
+              block = "music";
+              player = "spotify";
+              buttons = ["play" "next"];
+              on_collapsed_click = "spotify";
             }
             {
               block = "memory";
               display_type = "memory";
-              format_mem = "{mem_total_used_percents}%";
-              format_swap = "{swap_used_percents}%";
+              format_mem = "{mem_used;G}";
+              format_swap = "{swap_used;G}";
             }
             {
               block = "cpu";
               interval = 1;
-            }
-            {
-              block = "load";
-              interval = 1;
-              format = "{1m}";
+              format = "{barchart} {utilization}";
             }
             { block = "sound"; }
             {
