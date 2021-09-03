6a88493e2c03358ad093c22d742557118cb2e02a
sakulk <sakulk@gmail.com>
2021-09-03 19:03:52 +0200

    tty tweaks

--- hosts/tower.nix
+++ hosts/tower.nix
@@ -18,6 +18,7 @@
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.gfxpayloadBios = "keep";
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.extraModprobeConfig =
     "options hid_apple fnmode=2"; # enable F keys for Keychron K2
--- modules/base.nix
+++ modules/base.nix
@@ -30,8 +30,26 @@ in {
   time.timeZone = "Europe/Warsaw";
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
+    font = "lat2-16";
+    keyMap = "pl";
+    colors = [
+      "282828"
+      "cc241d"
+      "98971a"
+      "d79921"
+      "458588"
+      "b16286"
+      "689d6a"
+      "bdae93"
+      "7c6f64"
+      "fb4934"
+      "b8bb26"
+      "fabd2f"
+      "83a598"
+      "d3869b"
+      "8ec07c"
+      "ebdbb2"
+    ];
   };
 
   sound.enable = true;
