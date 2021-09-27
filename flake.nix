5d3bc20ecfbf879618d90bb7e4b4f593dfae3728
sakulk <sakulk@gmail.com>
2021-09-27 21:32:06 +0200

    refactor flake

--- flake.nix
+++ flake.nix
@@ -1,29 +1,34 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/master";
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = inputs: {
 
-    nixosConfigurations.saku-nixos = inputs.nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        inputs.home-manager.nixosModules.home-manager
-        "${inputs.nixpkgs}/nixos/modules/services/hardware/sane_extra_backends/brscan4.nix"
+    nixosConfigurations = {
+      saku-nixos = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          inputs.home-manager.nixosModules.home-manager
+          "${inputs.nixpkgs}/nixos/modules/services/hardware/sane_extra_backends/brscan4.nix"
 
-        ({ pkgs, ... }: {
-          nix.extraOptions = "experimental-features = nix-command flakes";
-          nix.package = pkgs.nixFlakes;
-          nix.registry.nixpkgs.flake = inputs.nixpkgs;
+          ({ pkgs, ... }: {
+            nix.extraOptions = "experimental-features = nix-command flakes";
+            nix.package = pkgs.nixFlakes;
+            nix.registry.nixpkgs.flake = inputs.nixpkgs;
 
-          home-manager.useGlobalPkgs = true;
-        })
+            home-manager.useGlobalPkgs = true;
+          })
 
-        ./hosts/tower.nix
-      ];
+          ./hosts/tower.nix
+        ];
+      };
     };
-
   };
 }
