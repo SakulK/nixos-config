974f6b2a5bad49b186120ead4e28c91f25f61011
sakulk <sakulk@gmail.com>
2021-11-09 20:54:28 +0100

    rust lsp in neovim

--- modules/base.nix
+++ modules/base.nix
@@ -429,6 +429,8 @@ in {
             tree-sitter-nix
             tree-sitter-rust
           ]))
+        nvim-lspconfig
+        lspsaga-nvim
       ];
       extraConfig = ''
         set hidden
@@ -462,6 +464,49 @@ in {
           },
         }
         require'indent_blankline'.setup()
+
+        local nvim_lsp = require('lspconfig')
+
+        -- Use an on_attach function to only map the following keys
+        -- after the language server attaches to the current buffer
+        local on_attach = function(client, bufnr)
+          local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+          local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+          -- Enable completion triggered by <c-x><c-o>
+          buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+          -- Mappings.
+          local opts = { noremap=true, silent=true }
+
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+          buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+          buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+          buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+          buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+          buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+          buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+          buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+          buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+          buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+          buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+        end
+
+        -- Use a loop to conveniently call 'setup' on multiple servers and
+        -- map buffer local keybindings when the language server attaches
+        local servers = { 'rust_analyzer' }
+        for _, lsp in ipairs(servers) do
+          nvim_lsp[lsp].setup {
+            on_attach = on_attach,
+            flags = {
+              debounce_text_changes = 150,
+            }
+          }
+        end
+
+        require'lspsaga'.init_lsp_saga()
         EOF
 
         let mapleader = " "
@@ -469,6 +514,13 @@ in {
         nnoremap <leader>fg <cmd>Telescope live_grep<cr>
         nnoremap <leader>fb <cmd>Telescope buffers<cr>
         nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+
+        nnoremap <silent> gd <cmd>lua require'lspsaga.provider'.preview_definition()<CR>
+        nnoremap <silent> gh <cmd>lua require'lspsaga.provider'.lsp_finder()<CR>
+        nnoremap <silent> K <cmd>lua require('lspsaga.hover').render_hover_doc()<CR>
+        nnoremap <silent><leader>ca <cmd>lua require('lspsaga.codeaction').code_action()<CR>
+        vnoremap <silent><leader>ca :<C-U>lua require('lspsaga.codeaction').range_code_action()<CR>
+        nnoremap <silent>gr <cmd>lua require('lspsaga.rename').rename()<CR>
       '';
     };
 
@@ -541,6 +593,7 @@ in {
     cargo
     rustc
     gcc
+    rust-analyzer
 
     # kubernetes
     kubectl
