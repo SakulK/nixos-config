f4f6425a8a17e09c8ef76b53deacb3280131e804
Łukasz Krenski <sakulk@gmail.com>
2022-03-19 19:19:59 +0100

    telescope resume bind

--- modules/base.nix
+++ modules/base.nix
@@ -512,6 +512,7 @@ in
         nnoremap <leader>fg <cmd>Telescope live_grep<cr>
         nnoremap <leader>fb <cmd>Telescope buffers<cr>
         nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+        nnoremap <leader>fr <cmd>Telescope resume<cr>
       '';
     };
 
