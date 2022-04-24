0cebe55c4e4a55ed2b4727c428b6ce276230431a
Łukasz Krenski <sakulk@gmail.com>
2022-04-24 15:49:55 +0200

    update

--- modules/base.nix
+++ modules/base.nix
@@ -51,7 +51,6 @@ in
   sound.enable = true;
   hardware.pulseaudio = {
     enable = true;
-    extraModules = [ pkgs.pulseaudio-modules-bt ];
     package = pkgs.pulseaudioFull;
     extraConfig = ''
       load-module module-switch-on-connect
