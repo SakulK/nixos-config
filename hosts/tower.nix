13a47e8bde18862af3f3fd7fac972aebc243c9cc
Łukasz Krenski <sakulk@gmail.com>
2025-05-03 08:13:08 +0200

    amdvlk

--- hosts/tower.nix
+++ hosts/tower.nix
@@ -68,6 +68,8 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = [ pkgs.amdvlk ];
+    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
   };
 
   services.minidlna = {
