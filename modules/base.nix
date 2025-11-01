a651bedcb25af8d94b17591c849f71f88d4adc41
Łukasz Krenski <sakulk@gmail.com>
2025-11-01 10:46:13 +0100

    use custom boxflat

--- modules/base.nix
+++ modules/base.nix
@@ -137,7 +137,7 @@ in
   services.udev.packages = [
     pkgs.chrysalis
     pkgs.bazecor
-    pkgs.boxflat
+    pkgs.nixpkgsSakulK.boxflat
   ];
   services.fwupd.enable = true;
 
@@ -753,6 +753,6 @@ in
     keepassxc
 
     python3
-    boxflat
+    nixpkgsSakulK.boxflat
   ];
 }
