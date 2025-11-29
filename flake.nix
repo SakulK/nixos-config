a69f35b0a3d95df09ddecc071290ffb14665777c
Łukasz Krenski <sakulk@gmail.com>
2025-11-29 12:18:49 +0100

    no boxflat override

--- flake.nix
+++ flake.nix
@@ -34,7 +34,6 @@
       let
         custom-overlay = final: prev: {
           stable = inputs.nixpkgs-stable.legacyPackages.${prev.system};
-          boxflat = inputs.nixpkgs-sakulk.legacyPackages.${prev.system}.boxflat;
           lcat = inputs.lcat.packages.x86_64-linux.default;
         };
 
